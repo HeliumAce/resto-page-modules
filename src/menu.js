6dfde8cb3ba1bff3548c03c4f288f3ca2cc4fce8
Liam Greig <helium.ace@gmail.com>
2020-06-02 11:40:32 -0400

    added menu logic

--- src/menu.js
+++ src/menu.js
@@ -16,6 +16,7 @@ const clearMainContent = () => {
 
 const updateMainContent = () => {
     createMenuGrid();
+    buildMenu();
     addCurrentMenu();
     setMenuStyles();
 }
@@ -31,24 +32,39 @@ const createMenuGrid = () => {
 }
 
 const addCurrentMenu = () => {
-    const schnitzel = MenuItem('schnitzel on a bun', '$10', 'Sugar Shack pork schnitzel, grainy mustard, mixed greens');
-    const deli = MenuItem('the marans deli sandwich', '$10', 'Selection of soft and hard cured meats, mustard aioli, mixed greens');
-    const tomato = MenuItem('toasted tomato and brie', '$10', 'Vine-ripened tomato, Brie, soy ginger vinaigrette, mixed greens');
-    const cheese = MenuItem('grilled cheese', '$8', 'All the cheese');
-    const ham = MenuItem('ham', '$8', 'All the ham');
-    
-    currentMenu.push(schnitzel, deli, tomato, cheese, ham);
-
-    for ( let i = 0; i < currentMenu.length; i++ ) {
-        currentMenu[i].render();
+    const menuCats = ['small plates', 'sandwiches', 'sides']
+    for ( let i = 0; i < menuCats.length; i++ ) {
+        let currentCat = document.createElement('div');
+        currentCat.setAttribute('id', menuCats[i]);
+        currentCat.setAttribute('class', 'menuCat');
+        currentCat.innerHTML = menuCats[i];
+        menu.appendChild(currentCat);
+        appendMenuItems(currentCat);
     }
 }
+
+const buildMenu = () => {
+    const schnitzel = MenuItem('small plates', 'schnitzel on a bun', '$10', 'Sugar Shack pork schnitzel, grainy mustard, mixed greens');
+    const deli = MenuItem('small plates', 'the marans deli sandwich', '$10', 'Selection of soft and hard cured meats, mustard aioli, mixed greens');
+    const tomato = MenuItem('sandwiches', 'toasted tomato and brie', '$10', 'Vine-ripened tomato, Brie, soy ginger vinaigrette, mixed greens');
+    const cheese = MenuItem('sandwiches', 'grilled cheese', '$8', 'All the cheese');
+    const ham = MenuItem('sides', 'ham', '$8', 'All the ham');    
+    currentMenu.push(schnitzel, deli, tomato, cheese, ham);
+}
+
+const appendMenuItems = (currentCat) => {
+    for ( let i = 0; i < currentMenu.length; i++ ) {  
+        if (currentCat.id === currentMenu[i].getCategory()) {
+            currentMenu[i].render();
+        }
+    }
+};
 
 const setMenuStyles = () => {
     //console.log('styles coming soon')
 };
 
-const MenuItem = (name, price, description) => {
+const MenuItem = (category, name, price, description) => {
     
     const render = () => {
         const item = document.createElement('div');
@@ -57,6 +73,7 @@ const MenuItem = (name, price, description) => {
         const itemDescription = document.createElement('div');
 
         item.setAttribute('id', 'menuItem');
+        item.setAttribute('class', category);
         itemName.setAttribute('id', 'itemName');
         itemPrice.setAttribute('id', 'itemPrice');
         itemDescription.setAttribute('id', 'itemDescription');
@@ -72,7 +89,9 @@ const MenuItem = (name, price, description) => {
 
     }
 
-    return { render };
+    const getCategory = () => category;
+
+    return { render, getCategory };
 };
 
 export { displayMenu };
